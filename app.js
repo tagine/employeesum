efc76f2ca62081a029bf3a136188405fea6c8d73
tagine <rana@ranafayez.com>
2020-02-05 16:05:41 -0500

    styling added and index html file created with containers

--- app.js
+++ app.js
@@ -1,21 +1,77 @@
 // Dependencies
 /// To ask questions
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
 /// To print to HTML file
-var fs = require("fs");
+const fs = require("fs");
 /// To test
-var jest = require("jest");
+const jest = require("jest");
 
+// push all newly constructed team members HERE
+const teamMembers = [];
+
+
+
+/// *NOTE TO SELF WOULDN'T YOU WANT A FILTER SO YOU CAN MAKE IT TO THE NEXT SET OF SPECIFIC QUESTIONS?
+// Question prompts using inquirer
+const askQuestions = () => [
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is your manager's name?"
+            // NOT NEEDED HERE but for others maybe//choices: ["yes","no"]
+        },
+
+        // ARE THERE MORE?
+
+        //ANSWERS IS SAVING ALL THE ANSWERS FROM THE QUESTIONS
+    ]).then(answers => {
+        const managerName = answers.managerName;
+        const manager = new Manager(managerName);
+    });
 
 // Variable declarations
-const engineer = {} ;
-const intern = {} ;
-const manager = {} ;
+///should the original values be in these variables right here instesad of the constructor?
+const employee =
+{
+    engineer: {},
+    intern: {},
+    manager: {},
+};
+
+// Functions
+// function getRole ();
+
+const outputPath = path.resolve(__dirname, "output", "team.html")
 
 // Classes
-class Engineer = {};
-class Intern = {};
-class Manager = {};
+/// Parent class
+class Employee {
+    name;
+    id;
+    title;
+    getName() { return name };
+    getId();
+    getEmail();
+    getRole(Engineer, Intern, Manager) {
 
+        // Subclass
+        class Engineer {
+            getGithub();
+            getRole();
+        };
+        // Subclass
+        class Intern {
+            school: "UPenn",
+            getSchool();
+            getRole();
+        };
+        // Subclass 
+        class Manager {
+            officeNumber: 200,
+            getRole();
+        }
+    };
+};
 
-
+const employee_variable = new Employee(name: "Rana", id: 0, title: "Engineer");
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Employee Summary Page
+ 
+## This is a commnad line generator application that builds a summary of an engineering team
+
+## User story
++ As a manager
++ I want to generate a webpage that displays my team's basic info
++ so that I have quick access to emails and GitHub profiles
